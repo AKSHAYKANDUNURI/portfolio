bdc488954482caf76265853ad1f56c0f40bcce86
AKSHAY <akshaykandunuri@gmail.com>
2025-02-07 13:42:21 +0530

    Update Home.js

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> AKSHAY KUMAR KANDUNURI</strong>
+                <strong className="main-name"> AKSHAY KUMAR KANDUNURI.</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
